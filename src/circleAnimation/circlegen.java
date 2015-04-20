bc25e7cdb2da88f97453324a7fc00035178f0bd6
Rajith Radhakrishnan <radrajith@gmail.com>
2015-04-19 22:11:59 -0400

    trying to display multiple circles

--- src/circleAnimation/circlegen.java
+++ src/circleAnimation/circlegen.java
@@ -9,22 +9,25 @@ public class circlegen extends JPanel{
 	final int size = 500;					//setting size for the frame and random location generator
 	static Random randomInt = new Random();
 	Color color = new Color(randomInt.nextInt(255),randomInt.nextInt(255),randomInt.nextInt(255));
-	public int zoom_circle = 0;
+	public int zoom_circle;
 	int posx = size/2;
 	int posy = size/2;
-	
-	public static int randNum(){
+	public int setZoom_circle(){			//increment zoom circle variable when called.
+		this.zoom_circle++;
+		return zoom_circle;
+	}
+	public static int randNum(){			//create a random radius values between 50- 150
 		int radius = randomInt.nextInt(100)+50;
 		return radius;
 	}
-	public static Color randColor(){
+	public static Color randColor(){		//generate random rgb color.
 		Color color = new Color(randomInt.nextInt(255),randomInt.nextInt(255),randomInt.nextInt(255));
 		return color;
 	}
 	
 	public circlegen(){}			//default constructor
 
-	public void makeShape(){		//drawing the shapes by calling the paintcomponent method
+	public void makeShape(){		//drawing the shapes by calling the paint component method
 		repaint();
 		
 	}
@@ -33,8 +36,9 @@ public class circlegen extends JPanel{
 		g.setColor(color);
 		int radius = circlegen.randNum();
 		g.fillOval(posx,posy, zoom_circle,zoom_circle);
-		if(zoom_circle>=100){
-			zoom_circle=0;
+		
+		if(zoom_circle>=100){		//check if the size = 100, then create a new color and new axis 
+			this.zoom_circle = 0;
 			color = new Color(randomInt.nextInt(255),randomInt.nextInt(255),randomInt.nextInt(255));
 			posx = randomInt.nextInt(size-radius);
 			posy = randomInt.nextInt(size-radius);
--- src/circleAnimation/test.java
+++ src/circleAnimation/test.java
@@ -7,20 +7,36 @@ public class test {
 	public static void main(String[] args){
 		//setting up the frame window
 		circlegen shape = new circlegen();
+		circlegen[] multiShape = new circlegen[10];
 		JFrame frame = new JFrame("Circle animations effect");
 		frame.setVisible(true);
 		frame.setSize(shape.size,shape.size);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(shape);
-		while(true){
-		shape.zoom_circle++;
-		shape.makeShape();
-		//delaying the loop by .05s
-		try {
-		    Thread.sleep(5);                 
-		} catch(InterruptedException ex) {
-		    Thread.currentThread().interrupt();
+		//creates objects for the array. 
+		for(int i=0; i<multiShape.length; i++){
+			multiShape[i] = new circlegen();
 		}
+		
+		while(true){
+			
+			//for(int i=1; i<multiShape.length; i++){
+				multiShape[0].setZoom_circle();
+				multiShape[0].makeShape();
+				System.out.println(multiShape[0].setZoom_circle());
+			//}
+			
+			
+			//shape.setZoom_circle();
+			//shape.makeShape();
+			
+		
+		//delaying the loop by .05s
+			try {
+				Thread.sleep(5);                 
+			} catch(InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
 		}
 	}
 }
